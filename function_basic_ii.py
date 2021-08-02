baab24f44355c13d85d676eaa5bf02ea7c11c1a0
Nick Fernandez <nbfern90@gmail.com>
2021-08-01 17:49:17 -0700

    update

---
+++ function_basic_ii.py
@@ -0,0 +1,17 @@
+# 1
+def countdown(x):
+    x.reverse()
+    return x
+
+
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+print(countdown(x))
+
+
+def count(x):
+    new = []
+    for i in new(x, 0, -1):
+        return x
+
+
+x = 5
