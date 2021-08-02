0951503321459c659ff6762d51407f1453313bc7
Nick Fernandez <nbfern90@gmail.com>
2021-08-01 19:02:15 -0700

    updated

--- function_basic_ii.py
+++ function_basic_ii.py
@@ -1,17 +1,52 @@
 # 1
 def countdown(x):
-    x.reverse()
-    return x
+    list = []
+    for i in range(x, -1, -1):
+        list.append(i)
+    return list
 
 
-x = [1, 2, 3, 4, 5, 6, 7, 8]
-print(countdown(x))
+print(countdown(5))
+
+# 2
 
 
-def count(x):
-    new = []
-    for i in new(x, 0, -1):
-        return x
+def newList(list):
+    print(list[0])
+    return list[1]
 
 
-x = 5
+print(newList([1, 2]))
+
+# 3
+
+
+def first_plus_length(list):
+    return list[0] + len(list)
+
+
+print(first_plus_length([1, 2, 3, 4, 5]))
+
+# 4
+
+
+def values_greater_than_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for i in range(0, len(list)):
+        if list[i] > list[1]
+        output.append(list[i])
+
+    print(len(output))
+    return output
+
+# 5
+
+
+def length_and_value(size, value):
+    output = []
+    for i in range(0, size,):
+        output.append(value)
+
+    return output
